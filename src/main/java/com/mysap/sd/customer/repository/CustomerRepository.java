115dcd7d0808e45be007e7d25a1c30384ea85889
Yu Chun,Chou <a830144@gmail.com>
2025-06-18 06:06:58 -0500

    Update CustomerRepository.java

--- src/main/java/com/mysap/sd/customer/repository/CustomerRepository.java
+++ src/main/java/com/mysap/sd/customer/repository/CustomerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     boolean existsByEmail(String email);
-    boolean existsByID(String id);
+    boolean existsById(String id);
 }
